b3fd5965ff5c64ae94b300d3d31326b28a548e8d
AythamiBR <aythami.86@gmail.com>
2024-05-09 18:54:43 +0100

    Done

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
-const menuToggle = document.getElementById("menu-toggle");
-const menuNav = document.getElementById("menu-nav");
+const menuToggle = document.getElementById("menu-toggle")
+const menuNav = document.getElementById("menu-nav")
 
 const toggleMenu = () => {
   console.log("called toggleMenu")
-  menuNav.classList.toggle("menu-toggle");
+  menuNav.classList.toggle("menu-toggle")
 }
 
-menuToggle.addEventListener("click", toggleMenu);
+menuToggle.addEventListener("click", toggleMenu)
